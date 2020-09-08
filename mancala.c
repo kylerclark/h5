98fa21882cfe1df46bf7bf88743c53e99a6d8eca
kc18 <kc18@iu.edu>
2020-09-08 19:35:57 -0400

    printBoard, started move

--- mancala.c
+++ mancala.c
@@ -4,7 +4,59 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+
+void printBoard(int board[2][6], int total1, int total2) {
+    printf("|%d|", total1);
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < 6; j++) {
+          printf("(%d)", board[i][j]);
+      }
+      if (i == 1) {
+          printf("|%d|\n   ", total2);
+      }
+      else {
+        printf("%s", "\n   ");
+      }
+    }
+}
+
+void move(int player, int choice, int board[2][6]) {
+    choice--;
+    player--;
+    int numStones = board[player][choice];
+    board[player][choice] = 0;
+    //player 1
+    if (player == 0) {
+      if (choice-numStones == -1) {
+        // extra turn
+        total1++;
+      }
+      else if (choice-numStones < -1) {
+        total1++;
+      }
+    }
+
+    //player 2
+    else if (player == 1) {
+
+    }
+    for (int i = choice+numStones; i > choice; i--) {
+      if (board[player][i] == )
+      board[player][i]++;
+    }
+}
 
 int main() {
-  
+
+    int board[2][6] = {
+      {4, 4, 4, 4, 4, 4},
+      {4, 4, 4, 4, 4, 4}
+      };
+
+    int total1 = 0;
+    int total2 = 0;
+
+    printBoard(board, total1, total2);
+
 }
