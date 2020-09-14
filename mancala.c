6d06bafb811ac4fa0f630d470a7f0db2beb3a97e
kc18 <kc18@iu.edu>
2020-09-13 22:05:42 -0400

    pretty much done

--- mancala.c
+++ mancala.c
@@ -8,8 +8,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int board1[6] = {4, 4, 4, 4, 4, 4};
-int board2[6] = {4, 4, 4, 4, 4, 4};
+int board[14] = {4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0};
 int total1 = 0;
 int total2 = 0;
 bool running = true;
@@ -17,162 +16,82 @@ bool p1Skip = false;
 bool p2Skip = false;
 
 void printBoard() {
-    if (total1 < 9) {
-      printf("|0%d|", total1);
+    if (total1 < 10) {
+        printf("|0%d|", total1);
     }
     else {
-      printf("|%d|", total1);
+        printf("|%d|", total1);
     }
-    for (int i = 0; i < 6; i++) {
-      if (board1[i] < 9) {
-        printf("(0%d)", board1[i]);
-      }
-      else {
-        printf("(%d)", board1[i]);
-      }
+
+    for (int i = 5; i >= 0; i--) {
+        if (board[i] < 10) {
+          printf("(0%d)", board[i]);
+        }
+        else {
+          printf("(%d)", board[i]);
+        }
     }
 
     printf("%c    ", '\n');
 
-    for (int i = 0; i < 6; i++) {
-      if (board2[i] < 9) {
-        printf("(0%d)", board2[i]);
-      }
-      else {
-        printf("(%d)", board2[i]);
-      }
+    for (int i = 7; i < 13; i++) {
+        if (board[i] < 10) {
+          printf("(0%d)", board[i]);
+        }
+        else {
+          printf("(%d)", board[i]);
+        }
     }
-    if (total2 < 9) {
-      printf("|0%d|\n", total2);
+
+    if (total2 < 10) {
+        printf("|0%d|\n", total2);
     }
     else {
-      printf("|%d|\n", total2);
+        printf("|%d|\n", total2);
     }
 }
 
 void move(int player) {
+    int choice = 0;
+    printf("Please enter a number (1-6): ");
+    scanf("%d", &choice);
 
-    //player 1
     if (player == 1) {
-      if (p1Skip) {
-        printf("%s\n", "P1 Skipped");
-      }
-      else {
-        int choice = 0;
-        printf("%s", "Player 1's turn. Enter choice(1-6): ");
-        scanf("%d", &choice);
-        choice--;
-        int numStones = board1[choice];
-        if (numStones == 0) {
-          move(1);
+        choice = 6-choice;
+    }
+    else {
+        choice += 6;
+    }
+    int stones = board[choice];
+    int stones2 = board[choice];
+    board[choice] = 0;
+
+    for(int i=choice+1; stones > 0; i++) {
+        if (i > 13) {
+            i = 0;
         }
-        board1[choice] = 0;
-        int extra = numStones-choice;
-        extra--;
-        if (extra == 0) {
-          total1++;
-          for(int i = choice-1; i >= choice-numStones+1; i--) {
-            board1[i]++;
-          }
-          printBoard();
-          move(player);
-        }
-        else if (extra <= -1) {
-          for(int i = choice-1; i >= choice-numStones; i--) {
-            board1[i]++;
-          }
-        }
-        else if (extra > -1) {
-          total1++;
-          extra--;
-          for(int i = choice-1; i >= 0; i--) {
-            board1[i]++;
-          }
-          if (extra <= 5) {
-            for(int i = 0; i <= extra; i++) {
-              board2[i]++;
-            }
-          }
-          else if (extra > 5) {
-            total2++;
-            extra--;
-            for(int i = 0; i < extra; i++) {
-              board2[i]++;
-            }
-            if (extra > 11) {
-              total2++;
-              extra--;
-              for(int i = 6; i >= 6-extra; i--) {
-                board1[i]++;
-              }
-            }
-            else {
-              for (int i = 6; i >= 11-extra; i--) {
-                board1[i]++;
-              }
-            }
-          }
-        }
-      }
+        board[i]++;
+        stones--;
     }
 
-    //player 2
-    if (player == 2) {
-      if (p2Skip) {
-        printf("%s\n", "P2 Skipped");
-      }
-      else {
-        int choice = 0;
-        printf("%s", "Player 2's turn. Enter choice(1-6): ");
-        scanf("%d", &choice);
-        choice--;
-        int numStones = board2[choice];
-        if (numStones == 0) {
-          move(2);
-        }
-        board2[choice] = 0;
-        int extra = numStones-(6-choice)+1;
-        if (extra == 1) {
-          total2++;
-          for(int i = choice+1; i < choice+numStones+1; i++) {
-            board2[i]++;
-          }
-          printBoard();
-          move(player);
-        }
-        else if (extra < 1) {
-          for(int i = choice+1; i < choice+numStones+1; i++) {
-            board2[i]++;
-          }
-        }
-        else if (extra > 1) {
-          total2++;
-          extra--;
-          for(int i = choice+1; i < 6; i++) {
-            board2[i]++;
-          }
-          if (extra < 6) {
-            for(int i = 6; i >= 6-extra; i--) {
-              board1[i]++;
-            }
-          }
-          else if (extra > 6) {
-            total1++;
-            extra--;
-            for(int i = 6; i > 6-extra; i--) {
-              board1[i]++;
-            }
-            if (extra > 12) {
-              total2++;
-              extra--;
-              for(int i = 6; i >= 6-extra; i--) {
-                board1[i]++;
-              }
-            }
-          }
-        }
-      }
+    total1 = board[6];
+    total2 = board[13];
+
+    if (player == 1 && 6-choice-stones2 == 0) {
+        system("cls");
+        printBoard();
+        printf("%s\n", "Player 1's Turn Again");
+        move(player);
+        return;
     }
+    if (player == 2 && choice+stones2 == 13) {
+        system("cls");
+        printBoard();
+        printf("%s\n", "Player 2's Turn Again");
+        move(player);
+        return;
+    }
+
 }
 
 void checkBoard() {
@@ -181,14 +100,18 @@ void checkBoard() {
     running = true;
     p1Skip = false;
     p2Skip = false;
-      for(int i = 0; i < 6;i++) {
-        if (board1[i] == 0) {
+
+    for (int i = 0; i < 6;i++) {
+        if (board[i] == 0) {
           count1++;
         }
-        else if (board2[i] == 0) {
+    }
+    for (int i = 7; i < 13; i++) {
+        if (board[i] == 0) {
           count2++;
         }
-      }
+    }
+
     if (count1 == 6 && count2 == 6) {
       running = false;
     }
@@ -201,21 +124,24 @@ void checkBoard() {
 }
 
 int main() {
+    system("cls");
     printBoard();
     while (running) {
-      checkBoard();
-      move(1);
-      system("cls");
-      printBoard();
-      move(2);
-      system("cls");
-      printBoard();
+        checkBoard();
+        printf("%s\n", "Player 1's Turn");
+        move(1);
+        system("cls");
+        printBoard();
+        printf("%s\n", "Player 2's Turn");
+        move(2);
+        system("cls");
+        printBoard();
     }
 
     if (total1 > total2) {
-      printf("%s", "Player 1 Wins!");
+        printf("%s", "Player 1 Wins!");
     }
     else {
-      printf("%s", "Player 2 Wins!");
+        printf("%s", "Player 2 Wins!");
     }
 }
